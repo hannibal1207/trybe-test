176a147dc818a4d8183568690ac5b7aa84a89b94
hannibal1207 <mathausannibal@gmail.com>
2021-11-05 02:33:38 -0300

    renderiza as informaçoes do banco de dado no front-end

--- todolist-front/src/pages/Task.js
+++ todolist-front/src/pages/Task.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 function Task() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/tasks')
+     .then(({ data }) => {
+       setTasks(data);
+     })
+     .catch(() => {
+       console.log('deu errado')
+     })
+  }, []);
   return (
     <div>
-      <h1>pagina para mostrar a task</h1>
-      <Link to="todo">
-        <button>Add new task</button>
-      </Link>
+      <div>
+        <h1>pagina para mostrar a task</h1>
+        <Link to="todo">
+          <button>Add new task</button>
+        </Link>
+      </div>
+      <div>
+        {tasks.map((data, key) => {
+          return(
+            <div>
+              <div>
+                <header>
+                  <h2 key={key}>Tarefa: {data.task}</h2>
+                  <button>Editar</button>
+                  <button>Excluir</button>
+                </header>
+              </div>
+           </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
